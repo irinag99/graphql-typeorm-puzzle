d636116fc715eac1e30fcd215871c340205f244f
irinag99 <iladinho@hotmail.com>
2020-09-22 20:51:38 -0300

    change for heroku

---
+++ src/app.ts
@@ -0,0 +1,50 @@
+import "reflect-metadata";
+import { createConnection } from "typeorm";
+import { ApolloServer } from "apollo-server-express";
+import express from 'express'
+import cors from "cors"
+import { buildSchema } from 'type-graphql';
+import * as dotenv from "dotenv";
+
+import { UserResolver } from "./resolvers/user"
+import { RecipeResolver } from "./resolvers/recipe";
+import { CategoryResolver } from "./resolvers/category";
+import { verifyUser } from '../src/helper/context/index';
+
+
+const startServer = async () => {
+  dotenv.config({ path: "../.env" });
+  await createConnection();
+  const schema = await buildSchema({
+    resolvers: [UserResolver, RecipeResolver, CategoryResolver]
+  });
+
+  const app = express();
+  app.use(cors())
+  app.use(express.json());
+
+  const server = new ApolloServer({
+    schema,
+    context: async ({ req }: any) => {
+      await verifyUser(req)
+      return {
+        email: req.email
+      }
+    }
+  });
+
+  server.applyMiddleware({ app, path: '/graphql' });
+
+  const PORT = process.env.PORT || 4000;
+
+  app.get('/', function (_req, res) {
+    res.send('Project');
+  });
+
+  app.listen(PORT, () => {
+    console.log(`server listening on PORT: ${PORT}`)
+    console.log(`Graphql Endpoint: ${server.graphqlPath}`)
+  });
+};
+
+startServer();
